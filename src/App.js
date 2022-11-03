136488c011408d4fd5053b2210d61b6da0d397d8
ajay <ap221882@gmail.com>
2022-11-03 21:53:13 +0530

    wrote test and code for toggling enable of button

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 
 function App() {
   const [color, setColor] = useState("red");
+  const [isDisabled, setIsDisabled] = useState(false);
   const newButtonColor = color === "red" ? "blue" : "red";
+  const handleButton = () => {
+    setIsDisabled(!isDisabled);
+  };
   return (
     <div>
       <button
         onClick={() => setColor(newButtonColor)}
         style={{ backgroundColor: color }}
+        disabled={isDisabled}
       >
         Change to {newButtonColor}
       </button>
+      <input onChange={handleButton} type='checkbox' />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { logRoles } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color and text", () => {
@@ -19,4 +18,27 @@ test("button when clicked turns blue and updates", () => {
   expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
   //expect text "Change to red"
   expect(colorButton).toHaveTextContent("Change to red");
+});
+
+// checkbox to disable toggle
+test("initial conditions", () => {
+  render(<App />);
+
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+// "Checkbox enables button on first click and disables on second click";
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox");
+  const button = screen.getByRole("button");
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
 });
