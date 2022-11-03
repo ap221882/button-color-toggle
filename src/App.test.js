cdb69d8fc03705be59d102eca67832e4e80782fe
ajay <ap221882@gmail.com>
2022-11-03 21:06:37 +0530

    wrote the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { logRoles } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color and text", () => {
-  const { container } = render(<App />);
-  logRoles(container);
+  render(<App />);
   // text
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
   // color
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
 
-test("button when clicked turns blue", () => {});
+test("button when clicked turns blue and updates", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  //click button
+  fireEvent.click(colorButton);
+  //expect color blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  //expect text "Change to red"
+  expect(colorButton).toHaveTextContent("Change to red");
+});
